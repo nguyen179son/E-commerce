20d05b75f776bf4d157d2310dd83c80ab274f24c
nguyen <sonnc@deha-soft.com>
2019-03-05 06:34:32 +0700

    change column type

---
+++ database/migrations/2019_03_04_231706_change_comments_table_columns_type.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeCommentsTableColumnsType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->integer('user_id')->change();
+            $table->integer('video_id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
